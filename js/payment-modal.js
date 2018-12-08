34f6b7a63fa323c51764273f9436ca624bd4df5e
RavenInd <emico.crow@gmail.com>
2018-12-08 18:10:55 +0300

    Clear Payment form

--- js/payment-modal.js
+++ js/payment-modal.js
@@ -4,6 +4,10 @@ window.payments = [];
 $('#paymentSubmit').click(function() {
     console.log('1');
     window.payments.push(collectPaymentData());
+    $('#paymentModal').modal('hide');
+    $("#paymentModal input").each(function(indx, element){
+        $(element).val("");
+      });
 });
 
 function collectPaymentData() {
